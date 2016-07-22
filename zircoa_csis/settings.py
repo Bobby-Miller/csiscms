5a83672fc494f62f4fcd368e7bca1334f8e9387f
Bobby Miller <miller.bobby@gmail.com>
2016-07-22 19:59:44 -0400

    Procfile Change?

--- zircoa_csis/settings.py
+++ zircoa_csis/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    # '/var/www/static/',
 )
 
 LOGIN_URL = '/login/'
