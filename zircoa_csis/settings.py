b26df391b7d8eb84b19594efed258c6b290d5f39
Bobby Miller <miller.bobby@gmail.com>
2016-07-22 20:18:43 -0400

    static change 2

--- zircoa_csis/settings.py
+++ zircoa_csis/settings.py
@@ -132,6 +132,21 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = '/var/www/static/'
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/' # It means home view
 
