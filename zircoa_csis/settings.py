e1bf310c0bcbd48b8c0017ed367722f4c122a60d
Bobby Miller <miller.bobby@gmail.com>
2016-07-22 20:16:22 -0400

    added static_root

--- zircoa_csis/settings.py
+++ zircoa_csis/settings.py
@@ -130,6 +130,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 )
+STATIC_ROOT = '/var/www/static/'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/' # It means home view
