b840e6f12042603f51fcb9dada9d66c501e506df
Bobby Miller <miller.bobby@gmail.com>
2016-07-24 08:32:07 -0400

    Updated databases setting

--- zircoa_csis/settings.py
+++ zircoa_csis/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
